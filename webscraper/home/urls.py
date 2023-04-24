15c7178abae3853e0f2b12cacb5df839941ba797
zahur <zahurmeerun@hotmail.com>
2023-04-24 15:43:04 +0100

    add: url to post send mail form

--- webscraper/home/urls.py
+++ webscraper/home/urls.py
@@ -3,4 +3,5 @@ from home import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('send_mail', views.send_mail, name='send_mail')
 ]
--- webscraper/home/views.py
+++ webscraper/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -6,3 +6,14 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+
+
+def send_mail(request):
+
+    if request.method == "POST":
+
+        print(request.POST.get('name'))
+        print(request.POST.get('email'))
+        print(request.POST.get('description'))
+
+        return redirect(reverse("home"))
